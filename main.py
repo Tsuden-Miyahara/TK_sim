f1204a5f72a03a204595580e092b3dd235ac776e
Tsuden-Miyahara <miyahara.tsuden@outlook.jp>
2023-09-14 15:59:33 +0900

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from math import radians as rad, degrees as deg, sin, cos, tan, asin, acos, atan
+from math import radians as rad, degrees as deg, sin, cos, tan, asin, atan2
 
-from util import get_poi_of_circle_with_line, get_theta2
+from util import LINE2D, line_circle_intersection, snell_s_law
 
 """
 パラメータを設定し光軸4の傾斜角γ4を計算する。
@@ -16,7 +16,207 @@ from util import get_poi_of_circle_with_line, get_theta2
 ⑥光軸4の傾斜角γ4を吟味する。
 """
 
-def calc(beta1, phy1, b_pos_x, b_pos_y, debug=True):
+class N:
+    AIR = 1
+    GLASS = 1.459
+    ACRYLIC_RESIN = 1.49
+
+def calc(b_pos_x: float, b_pos_y: float, beta_1: float, holder_r: float, tube_r: float, tube_thickness: float = 1):
+    """
+    :param b_pos_x: 照射位置 (X)
+    :param b_pos_y: 照射位置 (Y)
+    :param beta_1: β1の角度
+    :param holder_r: 液菅ホルダーの半径
+    :param tube_r: 液菅の半径
+    :param tube_thickness: 液菅の厚さ
+    :return: (θ6, θ6が平行になるときの角度)
+    """
+    b_pos = (b_pos_x, b_pos_y)
+    tube_center_pos = (0, -holder_r + tube_r)
+    tube_center_pos_x, tube_center_pos_y = tube_center_pos
+    
+    # 入射光 (line1) の式を求める
+    line1 = LINE2D.getFromPosAndDeg(*b_pos, beta_1)
+    
+    # 入射光と円の交点 (c1) の座標を求める
+    _c1_poses = line_circle_intersection(line1, *(0, 0), holder_r)
+    if not _c1_poses: raise Exception('入射光 (line1) がホルダーを通らない')
+    _, c1_pos = _c1_poses
+    c1_pos_x, c1_pos_y = c1_pos
+    
+    # α1を求める
+    _270deg_minus_alpha_1 = (deg( atan2(c1_pos_y - 0, c1_pos_x - 0) ) + 360) % 360
+    alpha_1 = 270 - _270deg_minus_alpha_1
+    
+    # θ1を求める
+    theta_1 = 90 - (alpha_1 + beta_1)
+    
+    # θ2を求める
+    theta_2, _ = snell_s_law(theta_1, N.ACRYLIC_RESIN, N.AIR)
+    if not theta_2: raise Exception('全反射（c1）')
+    
+    # 入射光 (line2) の式を求める
+    _line2_angle = 90 - (alpha_1 + theta_2)
+    line2 = LINE2D.getFromPosAndDeg(*c1_pos, _line2_angle)
+    
+    # 入射光と円の交点 (c2) の座標を求める
+    _c2_poses = line_circle_intersection(line2, *tube_center_pos, tube_r)
+    if not _c2_poses: raise Exception('入射光 (line2) がホルダーを通らない')
+    _, c2_pos = _c2_poses
+    c2_pos_x, c2_pos_y = c2_pos
+    
+    # α2を求める
+    _270deg_minus_alpha_2 = (deg( atan2(c2_pos_y - tube_center_pos_y, c2_pos_x - tube_center_pos_x) ) + 360) % 360
+    alpha_2 = 270 - _270deg_minus_alpha_2
+    
+    # θ3を求める
+    _x = 90 - (alpha_1 + theta_2)
+    theta_3 = 180 - (alpha_2 + 90 + _x)
+    
+    # θ4を求める
+    theta_4, _ = snell_s_law(theta_3, N.AIR, N.GLASS)
+    if not theta_4: raise Exception('全反射（c2）')
+    
+    # 入射光 (line3) の式を求める
+    _line3_angle = 90 - (alpha_2 + theta_4)
+    line3 = LINE2D.getFromPosAndDeg(*c2_pos, _line3_angle)
+    
+    # 入射光と円の交点 (A = c3) の座標を求める
+    _c3_poses = line_circle_intersection(line3, *tube_center_pos, tube_r - tube_thickness)
+    if not _c3_poses: raise Exception('入射光 (line3) がホルダーを通らない')
+    _, c3_pos = _c3_poses
+    c3_pos_x, c3_pos_y = c3_pos
+    
+    # α3を求める
+    _270deg_minus_alpha_3 = (deg( atan2(c3_pos_y - tube_center_pos_y, c3_pos_x - tube_center_pos_x) ) + 360) % 360
+    alpha_3 = 270 - _270deg_minus_alpha_3
+    
+    # θ5を求める
+    theta_5 = (90 - alpha_3) - (90 - alpha_2 - theta_4)
+    
+    # θ6を求める
+    theta_6, _ = snell_s_law(theta_5, N.GLASS, N.AIR)
+    if not theta_6: raise Exception('全反射（A = c3）')
+    
+    return (theta_6, 90 - alpha_3)
+
+
+
+
+class POS:
+    CENTER_MARGIN = 1
+    STATIC_CENTER = (-7.72, -13.29)
+    @classmethod
+    def CENTER(self, _):
+        return self.STATIC_CENTER
+    @classmethod
+    def UPPER(self, beta_1):
+        x, y = self.STATIC_CENTER
+        return (x - sin(beta_1) * self.CENTER_MARGIN, y + cos(beta_1) * self.CENTER_MARGIN)
+    @classmethod
+    def LOWER(self, beta_1):
+        x, y = self.STATIC_CENTER
+        return (x + sin(beta_1) * self.CENTER_MARGIN, y - cos(beta_1) * self.CENTER_MARGIN)
+
+
+if __name__ == '__main__':
+    import os
+    from matplotlib import pyplot
+    os.system('cls')
+
+    beta_1 = 40             # β1の初期値
+    step = 0.01             # 刻み
+    repeat = 10000          # 繰り返し
+    
+    tolerance = 0.5         # θ6の理想値と算出値の最大許容差
+    
+    # 液菅直径 [6 - 24.5]
+    tube_d = 24.5
+    # 入射点 [UPPER, CENTER, LOWER]
+    pos_target = POS.CENTER
+    
+    
+    X = []
+    Y1 = []
+    Y2 = []
+    OKAY = []
+    print(f"探索中 (β1: {beta_1} deg -> {beta_1 - repeat * step} deg [{step}刻み])")
+    is_valid = False
+    for n in range(repeat):
+        v = beta_1 - n * step
+        try:
+            THETA_6, THETA_6_IDEAL = calc(*pos_target(v), v, 12.7, tube_d / 2, 1)
+            print(f"θ6: {THETA_6}, θ6_id: {THETA_6_IDEAL}")
+            is_valid = True
+
+            THETA_6_DELTA = THETA_6_IDEAL - THETA_6
+            
+            X  += [v]
+            Y1 += [THETA_6]
+            Y2 += [THETA_6_IDEAL]
+            
+            if (abs(THETA_6_DELTA) < tolerance):
+                OKAY += [v]
+                if False:
+                    print(f"探索中断 [{n} / {repeat}] (β1: {v} deg)")
+                    break
+        except Exception as e:
+            if is_valid:
+                print(f"探索中断 [{n} / {repeat}] (β1: {v} deg): {e}")
+                break
+            print(f"スキップ [{n} / {repeat}] (β1: {v} deg): {e}")
+    l = len(X)
+    
+    _max: int = max(Y1 + Y2)
+    
+    pyplot.ylim(0, _max + 5)
+    if len(OKAY): pyplot.axvspan(min(OKAY), max(OKAY), color='gray')
+    pyplot.plot(X, Y1, '.-', label="calculated")
+    pyplot.plot(X, Y2, '.-', label="ideal")
+    pyplot.plot(X[l - 1], Y1[l - 1], '*')
+    pyplot.plot(X[l - 1], Y2[l - 1], '*')
+    pyplot.xlabel('β1 [deg]')
+    pyplot.ylabel('θ6 [deg]')
+    pyplot.legend()
+    pyplot.grid()
+    pyplot.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+
+def calc1(beta1, phy1, b_pos_x, b_pos_y, debug=True):
     # 1, 2
     BETA_1  = beta1            # 入射面交差角
     PHI_1   = phy1            # [mm] 液菅径
@@ -60,29 +260,6 @@ def calc(beta1, phy1, b_pos_x, b_pos_y, debug=True):
     return (BETA_1, THETA_1, THETA_2, GAMMA_2, THETA_1_CRIT, THETA_2_IDEAL)
 
 
-
-class POS:
-    CENTER_MARGIN = 1
-    STATIC_CENTER = (-7.72, -13.29)
-    @classmethod
-    def CENTER(self, _):
-        return self.STATIC_CENTER
-    @classmethod
-    def UPPER(self, beta_1):
-        x, y = self.STATIC_CENTER
-        return (x - sin(beta_1) * self.CENTER_MARGIN, y + cos(beta_1) * self.CENTER_MARGIN)
-    @classmethod
-    def LOWER(self, beta_1):
-        x, y = self.STATIC_CENTER
-        return (x + sin(beta_1) * self.CENTER_MARGIN, y - cos(beta_1) * self.CENTER_MARGIN)
-
-
-if __name__ == '__main__':
-    import os
-    from matplotlib import pyplot
-    os.system('cls')
-    
-        
     # 変数ここから
     base = 30.5             # β1の初期値
     step = 0.01             # 刻み
@@ -101,7 +278,7 @@ if __name__ == '__main__':
     print(f"探索中 (β1: {base} deg -> {base - repeat * step} deg [{step}刻み])")
     for n in range(repeat):
         v = base - n * step
-        _temp = calc(v, 24.5, *pos_target(v), False)
+        _temp = calc1(v, 24.5, *pos_target(v), False)
         beta_1, theta_1, theta_2, gamma_2, theta_1_crit, theta_2_ideal = _temp
         t2 = theta_2_ideal - theta_2
         
@@ -119,24 +296,28 @@ if __name__ == '__main__':
         RES = _temp
     
     beta_1, theta_1, theta_2, gamma_2, theta_1_crit, theta_2_ideal = RES
-    print(f"""
+    print(f\"""
 ======
 入射角 = {round(theta_1, 4)} deg
 (臨界まで: 約{round(theta_1_crit - theta_1, 3)} deg)
 屈折角 = {round(theta_2, 4)} deg
 (理想値との差: 約{round(theta_2_ideal - theta_2, 3)} deg)
 傾斜角 = {gamma_2}
-"""
+\"""
     )
     l = len(X)
     
-    pyplot.ylim(0, 90)
-    pyplot.plot(X, Y1, label="calculated")
-    pyplot.plot(X, Y2, label="ideal")
+    _max: int = max(Y1 + Y2)
+    
+    pyplot.ylim(0, _max + 5)
+    pyplot.plot(X, Y1, '.-', label="calculated")
+    pyplot.plot(X, Y2, '.-', label="ideal")
     pyplot.plot(X[l - 1], Y1[l - 1], '*')
     pyplot.plot(X[l - 1], Y2[l - 1], '*')
     pyplot.xlabel('β1 [deg]')
-    pyplot.ylabel('θ2')
+    pyplot.ylabel('θ2 [deg]')
     pyplot.legend()
     pyplot.grid()
     pyplot.show()
+
+"""
--- util.py
+++ util.py
@@ -1,6 +1,98 @@
 
 
-from math import sqrt, degrees as deg, radians as rad, sin, tan, asin
+from math import sqrt, degrees as deg, radians as rad, sin, tan, asin, atan2
+
+def snell_s_law(incident_angle: float, n1: float, n2: float):
+    """
+    スネルの法則を用いて屈折角を計算する関数
+    :param incident_angle: 入射角（度）
+    :param n1: 入射前の媒質の屈折率
+    :param n2: 入射後の媒質の屈折率
+    :return: (屈折角 [全反射の場合はNone], 臨界角 [存在しない場合はNone])。
+    """
+    assert 0 <= incident_angle <= 90, "incident_angle must be between 0 and 90 degrees"
+    assert n1 > 0 and n2 > 0, "n1 and n2 must be positive"
+    incident_angle_rad = rad(incident_angle)
+    critical_angle = None
+    if n1 > n2:
+        critical_angle_rad = asin(n2 / n1)
+        critical_angle = deg(critical_angle_rad)
+        if incident_angle_rad > critical_angle_rad:
+            return (None, critical_angle) # 全反射
+    refracted_angle = deg( asin( sin(incident_angle_rad) * n1 / n2 ) )
+    return (refracted_angle, critical_angle)
+
+class LINE2D:
+    # y = a * x + b
+    def __init__(self, a, b) -> None:
+        self.a: float = a
+        self.b: float = b
+    @classmethod
+    def getFromPosAndDeg(self, x, y, d):
+        a: float = tan(rad(d))
+        b: float = y - a * x
+        return LINE2D(a, b)
+    
+    def calcWithX(self, x):
+        return self.a * x + self.b
+    def calcWithY(self, y):
+        return (y - self.b) / self.a
+
+
+def line_circle_intersection(line: LINE2D, cx: float, cy: float, r: float):
+    # 直線の方程式を y = ax + b から x = (y - b) / a に変換
+    A = 1 + line.a**2
+    B = -2 * cx + 2 * line.a * (line.b - cy)
+    C = cx**2 + (line.b - cy)**2 - r**2
+
+    # 判別式
+    D = B**2 - 4 * A * C
+
+    if D < 0:
+        return None  # 実数解なし: 直線と円は交わらない
+    elif D == 0:
+        x: float = -B / (2 * A)
+        y: float = line.a * x + line.b
+        return [(x, y)]  # 実数解が一つ: 直線と円は接する
+    else:
+        x1: float = (-B + sqrt(D)) / (2*A)
+        y1: float = line.a*x1 + line.b
+        x2: float = (-B - sqrt(D)) / (2*A)
+        y2: float = line.a * x2 + line.b
+        return [(x1, y1), (x2, y2)]  # 実数解が二つ: 直線と円は交わる
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def get_theta2(n12, theta1):
     return deg( asin( sin(rad(theta1)) / n12 ) )
@@ -25,4 +117,6 @@ if __name__ == '__main__':
         *(0, -(7.72 * tan(rad(30.5))))
     )
     print(v)
-    print( get_theta2(1 / 1.459, 42.05) )
+    print(
+        snell_s_law(40.1, 1.459, 1)
+    )
